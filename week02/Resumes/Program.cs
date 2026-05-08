380ed0a6322b497acb4d05ffaa53d28f0ca34ff4
kimchristian50 <pchristison@byupathway.edu>
2026-05-08 14:02:55 -0600

    w02 resume homework

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Hewlett-Packard";
+        job1._jobTitle = "Manufacturing Development Engineer";
+        job1._startYear = 1996;
+        job1._endYear = 2000;
+
+        Job job2 = new Job();
+        job2._company = "Agilent Technologies";
+        job2._jobTitle = "Packaging Project Engineer";
+        job2._startYear = 2000;
+        job2._endYear = 2001;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Pam Christison";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
+
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,24 @@
+using System;
+
+// Resume
+// _name : string
+// _jobs : List<Job>
+// Display() : void
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+
+}

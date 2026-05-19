58b4a388404a0480280c60dec338293412c3787c
kimchristian50 <pchristison@byupathway.edu>
2026-05-19 12:17:01 -0600

    wo3 fractions program

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(6);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(1, 2);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.Write("What is the numerator? ");
+        int top = int.Parse(Console.ReadLine());
+        Console.Write("What is the denominator? ");
+        int bottom = int.Parse(Console.ReadLine());
+
+        Fraction f4 = new Fraction(top, bottom);
+        f4.SetTop(top);
+        f4.SetBottom(bottom);
+        Console.WriteLine($"top is {f4.GetTop()}");
+        Console.WriteLine($"bottom is {f4.GetBottom()}");
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }

11cdbe1d2390bd759fbfc496f56400a3802c10f1
kimchristian50 <pchristison@byupathway.edu>
2026-06-02 15:17:10 -0600

    homework assignment week05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assign1 = new Assignment("Thomas Jefferson", "Physics");
+
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment assign2 = new MathAssignment("John Adams", "geometry", "3.3", "1-99");
+        Console.WriteLine(assign2.GetMathSummary());
+
+        WritingAssignment assign3 = new WritingAssignment("Benjamin Franklin", "Writing 101", "Autobiography");
+        Console.WriteLine(assign3.GetWritingSummary());
     }
 }
